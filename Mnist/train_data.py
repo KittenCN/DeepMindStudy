3c7caa5ba11ea8a0bcd5b7372003c385b21884d1
KittenCN <owdely@vip.163.com>
2021-12-20 17:45:26 +0800

    add dvc

--- Mnist/train_data.py
+++ Mnist/train_data.py
@@ -15,34 +15,34 @@ test_loader = DataLoader(dataset = test_dataset, batch_size= 100, shuffle = True
 class mnist_net(nn.Module):
     def __init__(self) -> None:
         super(mnist_net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 64, 3, 1, 1)  # 28 * 28 
-        self.conv2 = nn.Conv2d(64, 128, 3, 1, 1)  # 28 * 28 
-        self.pool1 = nn.MaxPool2d(2, 2)  # 14 * 14
-        self.fc1 = nn.Linear(128 * 14 * 14, 1024)  
-        self.drop1 = nn.Dropout(p=0.5)
-        self.fc2 = nn.Linear(1024, 10)
+        # self.conv1 = nn.Conv2d(1, 64, 3, 1, 1)  # 28 * 28 
+        # self.conv2 = nn.Conv2d(64, 128, 3, 1, 1)  # 28 * 28 
+        # self.pool1 = nn.MaxPool2d(2, 2)  # 14 * 14
+        # self.fc1 = nn.Linear(128 * 14 * 14, 1024)  
+        # self.drop1 = nn.Dropout(p=0.5)
+        # self.fc2 = nn.Linear(1024, 10)
         # -----------------------------
-        # self.conv1 = nn.Sequential(nn.Conv2d(1, 64, 3, 1, 1),
-        #                             nn.ReLU(),
-        #                             nn.Conv2d(64, 128, 3, 1, 1),
-        #                             nn.ReLU(),
-        #                             nn.MaxPool2d(2, 2))
-        # self.dense = nn.Sequential(nn.Linear(128 * 14 * 14, 1024),
-        #                             nn.ReLU(),
-        #                             nn.Dropout(p=0.5),
-        #                             nn.Linear(1024, 10))
+        self.conv1 = nn.Sequential(nn.Conv2d(1, 64, 3, 1, 1),
+                                    nn.ReLU(),
+                                    nn.Conv2d(64, 128, 3, 1, 1),
+                                    nn.ReLU(),
+                                    nn.MaxPool2d(2, 2))
+        self.dense = nn.Sequential(nn.Linear(128 * 14 * 14, 1024),
+                                    nn.ReLU(),
+                                    nn.Dropout(p=0.5),
+                                    nn.Linear(1024, 10))
     
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = self.pool1(F.relu(self.conv2(x)))
-        x = x.view(-1, 128 * 14 * 14)
-        x = F.relu(self.fc1(x))
-        x = self.drop1(x)
-        x = self.fc2(x)
-        # -----------------------------
-        # x = self.conv1(x)
+        # x = F.relu(self.conv1(x))
+        # x = self.pool1(F.relu(self.conv2(x)))
         # x = x.view(-1, 128 * 14 * 14)
-        # x = self.dense(x)
+        # x = F.relu(self.fc1(x))
+        # x = self.drop1(x)
+        # x = self.fc2(x)
+        # -----------------------------
+        x = self.conv1(x)
+        x = x.view(-1, 128 * 14 * 14)
+        x = self.dense(x)
         return x
 
 net = mnist_net().to(device)
