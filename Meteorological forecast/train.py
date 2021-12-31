28c3689e6c54be78819f95681d36965930285528
Peng Lv <owdely@vip.163.com>
2021-12-31 10:44:17 +0800

    temp

--- Meteorological forecast/train.py
+++ Meteorological forecast/train.py
@@ -1,179 +1,273 @@
-from torch.utils.data.dataset import TensorDataset
-import dbhelper as db
+from __future__ import print_function
+import argparse
+import os
+import random
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils.data import DataLoader
-import numpy as np
-import math
-import os
-from tqdm import tqdm
-from prefetch_generator import BackgroundGenerator
-from apex import amp
-# from torch.utils.tensorboard import SummaryWriter
-# import torchvision
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
 
-db_path = r"Meteorological forecast/data/DB/database.db"
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-ori_in_date = []
-ori_out_data = [] 
-_db = db.Connect(db_path)
-pklfile = r"Meteorological forecast/model/model.pkl"
-# log_writer = SummaryWriter(r"/root/tf-logs/")
 
-class DataLoaderX(DataLoader):
-    def __iter__(self):
-        return BackgroundGenerator(super().__iter__())
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', required=True, help='cifar10 | lsun | mnist |imagenet | folder | lfw | fake')
+parser.add_argument('--dataroot', required=False, help='path to dataset')
+parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
+parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
+parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
+parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
+parser.add_argument('--ngf', type=int, default=64)
+parser.add_argument('--ndf', type=int, default=64)
+parser.add_argument('--niter', type=int, default=25, help='number of epochs to train for')
+parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
+parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
+parser.add_argument('--cuda', action='store_true', help='enables cuda')
+parser.add_argument('--dry-run', action='store_true', help='check a single training cycle works')
+parser.add_argument('--ngpu', type=int, default=1, help='number of GPUs to use')
+parser.add_argument('--netG', default='', help="path to netG (to continue training)")
+parser.add_argument('--netD', default='', help="path to netD (to continue training)")
+parser.add_argument('--outf', default='.', help='folder to output images and model checkpoints')
+parser.add_argument('--manualSeed', type=int, help='manual seed')
+parser.add_argument('--classes', default='bedroom', help='comma separated list of classes for the lsun data set')
 
-class net(nn.Module):
-    def __init__(self) -> None:
-        super(net, self).__init__()
-        self.fc1 = nn.Linear(5, 128)
-        self.fc2 = nn.Linear(128, 512)
-        self.fc3 = nn.Linear(512, 256)
-        self.fc4 = nn.Linear(256, 64)
-        self.fc5 = nn.Linear(64, 1)
-    
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = F.relu(self.fc4(x))
-        x = self.fc5(x)
-        return x
+opt = parser.parse_args()
+print(opt)
 
-def RP(t, h):
-    try:
-        a = 17.27
-        b = 237.7
-        y = ((a * t) / (b + t)) + math.log(h / 100)
-        Td = (b * y) / (a - y)
-        return Td
-    except BaseException:
-        print(t, h)
-        exit()
+try:
+    os.makedirs(opt.outf)
+except OSError:
+    pass
 
-def checkdata(num):
-    if num >= 30000:
-        return False
-    return True
+if opt.manualSeed is None:
+    opt.manualSeed = random.randint(1, 10000)
+print("Random Seed: ", opt.manualSeed)
+random.seed(opt.manualSeed)
+torch.manual_seed(opt.manualSeed)
 
-class rainfall:
-    def __init__(self, id, allday_rainfall):
-        self.id = id
-        self.allday_rainfall = allday_rainfall
+cudnn.benchmark = True
 
-class metedata:
-    def __init__(self, id, avg_temp, avg_humidity, avg_pressure, altitude):
-        self.id = id
-        self.avg_temp = avg_temp
-        self.avg_humidity = avg_humidity
-        self.avg_pressure = avg_pressure
-        self.altitude = altitude
+if torch.cuda.is_available() and not opt.cuda:
+    print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+  
+if opt.dataroot is None and str(opt.dataset).lower() != 'fake':
+    raise ValueError("`dataroot` parameter is required for dataset \"%s\"" % opt.dataset)
 
-if __name__ == "__main__":
-    unvalidID = []
-    rainfalllist = []
-    metedatalist = []
-    # strSQL = "select a.locationID, a.altitude, a.year, a.month, a.day, a.allday_rainfall from pre a inner join rhu b inner join tem c on a.locationID = b.locationID and a.locationID = c.locationID and a.year = b.year and a.year = c.year and a.month = b.month and a.month = c.month and a.day = b.day and a.day = c.day where 1 = 1 order by a.locationID, a.year, a.month, a.day"
-    # _datas = _db.query(strSQL, True)
-    # for i, dt in enumerate(_datas):
-    #     if checkdata(int(dt[4])) == False and i not in unvalidID:
-    #         unvalidID.append(i)
-    #     rainfalllist.append(rainfall(i, dt[4])) 
-    # strSQL = "select count(a.locationID) from prs a inner join rhu b inner join tem c inner join pre d on a.locationID = b.locationID and a.locationID = c.locationID and a.locationID = d.locationID and a.year = b.year and a.year = c.year and a.year = d.year and a.month = b.month and a.month = c.month and a.month = d.month and a.day = b.day and a.day = c.day and a.day = d.day where a.avg_pressure < 30000 and b.avg_humidity < 30000 and c.avg_temp < 3000 and d.allday_rainfall < 3000 and a.altitude < 90000 order by a.locationID, a.year, a.month, a.day"
-    # # strSQL += " limit 1000"
-    # _datas = _db.query(strSQL, True)
-    # for dt in _datas:
-    #     rowcnt = dt[0]
-    #     break
-    # strSQL = "select a.locationID, a.altitude, a.year, a.month, a.day, a.avg_pressure, b.avg_humidity, c.avg_temp, d.allday_rainfall from prs a inner join rhu b inner join tem c inner join pre d on a.locationID = b.locationID and a.locationID = c.locationID and a.locationID = d.locationID and a.year = b.year and a.year = c.year and a.year = d.year and a.month = b.month and a.month = c.month and a.month = d.month and a.day = b.day and a.day = c.day and a.day = d.day where a.avg_pressure < 30000 and b.avg_humidity < 30000 and c.avg_temp < 3000 and d.allday_rainfall < 3000 and a.altitude < 90000 order by a.locationID, a.year, a.month, a.day"
-    # _datas = _db.query(strSQL, True)
-    # strSQL += " limit 1000"
-    _table = _db.table("METE")
-    _datas = _table.findAll()
-    rowcnt = len(_datas)
-    subbar = tqdm(total=rowcnt)
-    for i, dt in enumerate(_datas):
-        subbar.update(1)
-        if (checkdata(int(dt['avg_pressure'])) == False or checkdata(int(dt['avg_humidity'])) == False or checkdata(int(dt['avg_temp'])) == False or checkdata(int(dt['allday_rainfall'])) == False or int(dt['avg_humidity']) <= 0) and i not in unvalidID:
-            unvalidID.append(i)
-        rainfalllist.append(rainfall(i, dt['avg_temp'])) 
-        metedatalist.append(metedata(i, dt['avg_temp'], dt['avg_humidity'], dt['avg_pressure'], dt['altitude']))
-    subbar.close()
-    subbar = tqdm(total=len(rainfalllist))
-    for i, dt in enumerate(rainfalllist):
-        subbar.update(1)
-        if i in unvalidID:
-            continue
-        tempdt = []
-        if int(dt.allday_rainfall) > 0:
-            tempdt.append(1)
+if opt.dataset in ['imagenet', 'folder', 'lfw']:
+    # folder dataset
+    dataset = dset.ImageFolder(root=opt.dataroot,
+                               transform=transforms.Compose([
+                                   transforms.Resize(opt.imageSize),
+                                   transforms.CenterCrop(opt.imageSize),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                               ]))
+    nc=3
+elif opt.dataset == 'lsun':
+    classes = [ c + '_train' for c in opt.classes.split(',')]
+    dataset = dset.LSUN(root=opt.dataroot, classes=classes,
+                        transform=transforms.Compose([
+                            transforms.Resize(opt.imageSize),
+                            transforms.CenterCrop(opt.imageSize),
+                            transforms.ToTensor(),
+                            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                        ]))
+    nc=3
+elif opt.dataset == 'cifar10':
+    dataset = dset.CIFAR10(root=opt.dataroot, download=True,
+                           transform=transforms.Compose([
+                               transforms.Resize(opt.imageSize),
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                           ]))
+    nc=3
+
+elif opt.dataset == 'mnist':
+        dataset = dset.MNIST(root=opt.dataroot, download=True,
+                           transform=transforms.Compose([
+                               transforms.Resize(opt.imageSize),
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.5,), (0.5,)),
+                           ]))
+        nc=1
+
+elif opt.dataset == 'fake':
+    dataset = dset.FakeData(image_size=(3, opt.imageSize, opt.imageSize),
+                            transform=transforms.ToTensor())
+    nc=3
+
+assert dataset
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize,
+                                         shuffle=True, num_workers=int(opt.workers))
+
+device = torch.device("cuda:0" if opt.cuda else "cpu")
+ngpu = int(opt.ngpu)
+nz = int(opt.nz)
+ngf = int(opt.ngf)
+ndf = int(opt.ndf)
+
+
+# custom weights initialization called on netG and netD
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        torch.nn.init.normal_(m.weight, 1.0, 0.02)
+        torch.nn.init.zeros_(m.bias)
+
+
+class Generator(nn.Module):
+    def __init__(self, ngpu):
+        super(Generator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d(     nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            # state size. (ngf*2) x 16 x 16
+            nn.ConvTranspose2d(ngf * 2,     ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            # state size. (ngf) x 32 x 32
+            nn.ConvTranspose2d(    ngf,      nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # state size. (nc) x 64 x 64
+        )
+
+    def forward(self, input):
+        if input.is_cuda and self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
         else:
-            tempdt.append(0)
-        ori_out_data.append(tempdt)
-    subbar.close()
-    subbar = tqdm(total=len(metedatalist))
-    for i, dt in enumerate(metedatalist):
-        subbar.update(1)
-        if i in unvalidID:
-            continue
-        tempdt = []
-        tempdt.append(float(int(dt.avg_temp)) / 10)
-        tempdt.append(float(int(dt.avg_humidity)))
-        tempdt.append(float(int(dt.avg_pressure)) / 100)
-        tempdt.append(float(int(dt.altitude)) / 10)
-        tempdt.append(RP(float(int(dt.avg_temp)) / 10, float(int(dt.avg_humidity))))
-        ori_in_date.append(tempdt)    
-    _db.close()
-    subbar.close()
-    # del ori_in_date[-1]
-    # del ori_out_data[0]
-    epochs = 100000
-    lr = 0.01
-    if os.path.exists(pklfile):
-#         model = torch.load(pklfile).to(device)
-        model = net().to(device) 
-        model.load_state_dict(torch.load(pklfile))
-        model.eval()
-        print("load old model")
-    else:
-        model = net().to(device)
-        print("creat new model")
-    print("total data: ", rowcnt, "valid data: ", len(ori_in_date))
-    in_data = torch.from_numpy(np.array(ori_in_date)).float()
-    out_data = torch.from_numpy(np.array(ori_out_data)).float()
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    loss_func = nn.MSELoss().to(device)
-    model, optimizer = amp.initialize(model, optimizer, opt_level="O0")
-    dataset = TensorDataset(in_data, out_data)
-    # data_loader = DataLoader(dataset, batch_size=1024, shuffle=True, num_workers=16, pin_memory=True)
-    data_loader = DataLoaderX(dataset, batch_size=1024, shuffle=True, num_workers=16, pin_memory=True)
-    bar = tqdm(total=10, leave=False)
-    for epoch in range(epochs):
-        bar.update(1)
-        subbar = tqdm(total=len(data_loader), leave=False)
-        for inputs, targets in data_loader:
-            subbar.update(1)
-            inputs = inputs.to(device)
-            targets = targets.to(device)
-            outputs = model(inputs)
-            # grid = torchvision.utils.make_grid(inputs)
-            # log_writer.add_image('inputs', grid, 0)
-            # log_writer.add_graph(model, inputs)
-            loss = loss_func(outputs, targets.to(device))
-            # log_writer.add_scalar('Loss/train', float(loss), epoch)
-            optimizer.zero_grad()
-            with amp.scale_loss(loss, optimizer) as scaled_loss:
-                scaled_loss.backward()
-            # loss.backward()
-            optimizer.step()
-        subbar.close()
-        if (epoch + 1) % 10 == 0 and epoch != 0:
-            bar.close()
-            print('epoch:', epoch + 1, 'loss:', loss.item())
-            bar = tqdm(total=10, leave=False)
-        if (epoch + 1) % 100 == 0 and epoch != 0:
-            torch.save(model.state_dict(), pklfile)
-    # log_writer.close()
+            output = self.main(input)
+        return output
 
+
+netG = Generator(ngpu).to(device)
+netG.apply(weights_init)
+if opt.netG != '':
+    netG.load_state_dict(torch.load(opt.netG))
+print(netG)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, ngpu):
+        super(Discriminator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input is (nc) x 64 x 64
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x 32 x 32
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 16 x 16
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 8 x 8
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*8) x 4 x 4
+            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input):
+        if input.is_cuda and self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+        else:
+            output = self.main(input)
+
+        return output.view(-1, 1).squeeze(1)
+
+
+netD = Discriminator(ngpu).to(device)
+netD.apply(weights_init)
+if opt.netD != '':
+    netD.load_state_dict(torch.load(opt.netD))
+print(netD)
+
+criterion = nn.BCELoss()
+
+fixed_noise = torch.randn(opt.batchSize, nz, 1, 1, device=device)
+real_label = 1
+fake_label = 0
+
+# setup optimizer
+optimizerD = optim.Adam(netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+optimizerG = optim.Adam(netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+
+if opt.dry_run:
+    opt.niter = 1
+
+for epoch in range(opt.niter):
+    for i, data in enumerate(dataloader, 0):
+        ############################
+        # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+        ###########################
+        # train with real
+        netD.zero_grad()
+        real_cpu = data[0].to(device)
+        batch_size = real_cpu.size(0)
+        label = torch.full((batch_size,), real_label,
+                           dtype=real_cpu.dtype, device=device)
+
+        output = netD(real_cpu)
+        errD_real = criterion(output, label)
+        errD_real.backward()
+        D_x = output.mean().item()
+
+        # train with fake
+        noise = torch.randn(batch_size, nz, 1, 1, device=device)
+        fake = netG(noise)
+        label.fill_(fake_label)
+        output = netD(fake.detach())
+        errD_fake = criterion(output, label)
+        errD_fake.backward()
+        D_G_z1 = output.mean().item()
+        errD = errD_real + errD_fake
+        optimizerD.step()
+
+        ############################
+        # (2) Update G network: maximize log(D(G(z)))
+        ###########################
+        netG.zero_grad()
+        label.fill_(real_label)  # fake labels are real for generator cost
+        output = netD(fake)
+        errG = criterion(output, label)
+        errG.backward()
+        D_G_z2 = output.mean().item()
+        optimizerG.step()
+
+        print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
+              % (epoch, opt.niter, i, len(dataloader),
+                 errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+        if i % 100 == 0:
+            vutils.save_image(real_cpu,
+                    '%s/real_samples.png' % opt.outf,
+                    normalize=True)
+            fake = netG(fixed_noise)
+            vutils.save_image(fake.detach(),
+                    '%s/fake_samples_epoch_%03d.png' % (opt.outf, epoch),
+                    normalize=True)
+
+        if opt.dry_run:
+            break
+    # do checkpointing
+    torch.save(netG.state_dict(), '%s/netG_epoch_%d.pth' % (opt.outf, epoch))
+    torch.save(netD.state_dict(), '%s/netD_epoch_%d.pth' % (opt.outf, epoch))
