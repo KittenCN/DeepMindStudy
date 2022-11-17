4b9e6b9c14d64a7d57332e72b30ed46ed89ec1ea
KittenCN <owdely@vip.163.com>
2022-11-17 18:06:12 +0800

    new files

--- test.py
+++ test.py
@@ -1,3 +1,25 @@
-x = int(input())
-if x < 0:
-    raise Exception("Sorry, no numbers below zero")
+class people:
+    name = ''
+    age = 0
+    __weight = 0
+
+    def __init__(self, n, a, w):
+        self.name = n
+        self.age = a
+        self.__weight = w
+    
+    def speak(self):
+        print("%s 说: 我 %d 岁。" %(self.name, self.age))
+
+class student(people):
+    grade = ''
+
+    def __init__(self, n, a, w, g):
+        people.__init__(self, n, a, w)
+        self.grade = g
+    
+    def speak(self):
+        print("%s 说: 我 %d 岁了，我在读 %d 年级" %(self.name, self.age, self.grade))
+
+s = student('ken', 10, 60, 3)
+s.speak()
